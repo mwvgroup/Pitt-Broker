8e4f7267078ff8096676ca7a87323f0fa0abf795
Daniel <djperrefort@gmail.com>
2020-08-07 14:45:06 -0400

    Groups setup tasks by associated service

--- broker/gcp_setup.py
+++ broker/gcp_setup.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 # -*- coding: UTF-8 -*-
 
-"""The ``gcp_setup`` module sets up services on the Google Cloud Platform (GCP
-project for use by the parent package. Setup tasks can be run individually or
-collectively using the ``auto_setup`` function. Each task should only have to
-be run once for a given GCP project.
+"""The ``gcp_setup`` module sets up services on the Google Cloud Platform (GCP)
+for use by the parent package. Setup tasks are grouped together into functions
+that configure resources required to a particular task. Resources configured
+by a given function are listed in the documentation.
 
 Usage Example
 -------------
@@ -14,14 +14,14 @@ Usage Example
 
    from broker import gcp_setup
 
-   # Tasks can be run individually
-   gcp_setup.setup_big_query
-   gcp_setup.setup_buckets
+   # Set of logging for the parent package
    gcp_setup.setup_logging_sinks
 
-   # Tasks can also be run as a collective set.
-   gcp_setup.auto_setup
+   # Set up resources used by the test suite
+   gcp_setup.setup_testsuite_resources
 
+   # Setup resources used when ingesting ZTF alerts
+   gcp_setup.setup_ztf_ingest_resources
 
 Module Documentation
 --------------------
@@ -34,59 +34,74 @@ if not os.getenv('GPB_OFFLINE', False):
     from google.api_core.exceptions import NotFound
     from google.cloud import bigquery, pubsub, logging, storage
 
-PROJECT_ID = os.getenv('GOOGLE_CLOUD_PROJECT')
-
 _tables = ('alerts', 'test_GCS_to_BQ')
+bigquery_client = bigquery.Client()
+storage_client = storage.Client()
 
 
-def setup_big_query() -> None:
-    """Create the necessary Big Query datasets if they do not already exist
+def _create_bucket(bucket: str) -> None:
+    """Create a storage bucket if it does not already exist:
+
+    Args:
+       bucket: Name of the bucket it it does not
+    """
+
+    try:
+        storage_client.get_bucket(bucket)
+
+    except NotFound:  # Create buckets if they do not exist
+        storage_client.create_bucket(bucket)
+
+
+def setup_logging_sinks() -> None:
+    """Create sinks for exporting log entries to GCP
+
+    New sinks include:
+      - ``broker_logging_sink``
+    """
+
+    # Define logging sink
+    logging_client = logging.Client()
+    sink = logging_client.sink('logging_sink')
+
+    # Create sink if not exists
+    if not sink.exists():
+        sink.create()
+
+
+def setup_testsuite_resources() -> None:
+    """Set up GCP resources necessary for running the test suite
 
     New datasets include:
-      ``ztf_alerts``
-      ``testing_dataset``
-    """
-
-    bigquery_client = bigquery.Client()
-    bigquery_client.create_dataset('ztf_alerts', exists_ok=True)
-    bigquery_client.create_dataset('testing_dataset', exists_ok=True)
-
-
-def setup_buckets() -> None:
-    """Create new storage buckets and upload testing files.
-    Files are expected to reside in the ``tests/test_alerts`` directory.
+      - ``testing_dataset``
 
     New buckets [files] include:
-      ``<PROJECT_ID>_ztf_alert_avro_bucket``
-      ``<PROJECT_ID>_testing_bucket`` [``ztf_3.3_validschema_1154446891615015011.avro``]
+      - ``<PROJECT_ID>_testing_bucket``  [``ztf_3.3_validschema_1154446891615015011.avro``]
     """
 
-    buckets = {  # '<bucket name>': ['file name',]
-                f'{PROJECT_ID}_ztf_alert_avro_bucket': [],
-                f'{PROJECT_ID}_testing_bucket':
-                    ['ztf_3.3_validschema_1154446891615015011.avro']
-                }
+    bigquery_client.create_dataset('testing_dataset', exists_ok=True)
 
-    storage_client = storage.Client()
+    testing_bucket = f'{os.getenv("GOOGLE_CLOUD_PROJECT")}_testing_bucket'
+    _create_bucket(testing_bucket)
 
-    for bucket_name, files in buckets.items():
-        # Create buckets if they do not exist
-        try:
-            storage_client.get_bucket(bucket_name)
-        except NotFound:
-            storage_client.create_bucket(bucket_name)
-
-        # Upload any files
-        for filename in files:
-            bucket = storage_client.get_bucket(bucket_name)
-            blob = bucket.blob(filename)
-            inpath = Path('tests/test_alerts') / filename
-            with inpath.open('rb') as infile:
-                blob.upload_from_file(infile)
+    # Upload any files
+    testing_files = ['ztf_3.3_validschema_1154446891615015011.avro']
+    for filename in testing_files:
+        bucket = storage_client.get_bucket(testing_bucket)
+        blob = bucket.blob(filename)
+        inpath = Path('tests/test_alerts') / filename
+        with inpath.open('rb') as infile:
+            blob.upload_from_file(infile)
 
 
-def setup_pubsub() -> None:
-    """ Create new Pub/Sub topics and subscriptions
+def setup_ztf_ingest_resources() -> None:
+    """Set up GCP resources for consuming ZTF alerts
+
+    New datasets include:
+      - ``ztf_alerts``
+
+    New buckets [files] include:
+      - ``<PROJECT_ID>_ztf_alert_avro_bucket``
 
     New topics [subscriptions] include:
         ``ztf_alert_data``
@@ -94,21 +109,24 @@ def setup_pubsub() -> None:
         ``ztf_alerts_in_BQ``
         ``test_alerts_in_BQ``
         ``test_alerts_PS_publish`` [``test_alerts_PS_subscribe``]
+
     """
 
-    topics = {# '<topic_name>': ['<subscription_name>', ]
-                'ztf_alert_data': [],
-                'ztf_alert_avro_in_bucket': [],
-                'ztf_alerts_in_BQ': [],
-                'test_alerts_in_BQ': [],
-                'test_alerts_PS_publish': ['test_alerts_PS_subscribe']
-                }
+    bigquery_client.create_dataset('ztf_alerts', exists_ok=True)
 
+    topics = {  # '<topic_name>': ['<subscription_name>', ]
+        'ztf_alert_data': [],
+        'ztf_alert_avro_in_bucket': [],
+        'ztf_alerts_in_BQ': [],
+        'test_alerts_in_BQ': [],
+        'test_alerts_PS_publish': ['test_alerts_PS_subscribe']
+    }
+
+    project_id = os.getenv("GOOGLE_CLOUD_PROJECT")
     publisher = pubsub.PublisherClient()
     subscriber = pubsub.SubscriberClient()
-
     for topic, subscriptions in topics.items():
-        topic_path = publisher.topic_path(PROJECT_ID, topic)
+        topic_path = publisher.topic_path(project_id, topic)
 
         # Create the topic
         try:
@@ -118,52 +136,8 @@ def setup_pubsub() -> None:
 
         # Create any subscriptions:
         for sub_name in subscriptions:
-            sub_path = subscriber.subscription_path(PROJECT_ID, sub_name)
+            sub_path = subscriber.subscription_path(project_id, sub_name)
             try:
                 subscriber.get_subscription(sub_path)
             except NotFound:
                 subscriber.create_subscription(sub_path, topic_path)
-
-
-def setup_logging_sinks() -> None:
-    """Create sinks for exporting log entries to GCP
-
-    This function assumes destination buckets have already been created.
-
-    New Sinks include:
-      ``broker_logging_sink``
-    """
-
-    # Define logging sink
-    logging_client = logging.Client()
-    logging_sink_name = 'logging_sink'
-    sink = logging_client.sink(logging_sink_name)
-
-    # Create sink if not exists
-    if not sink.exists():
-        sink.create()
-
-
-def auto_setup() -> None:
-    """Create and setup GCP products required by the ``broker`` package
-
-    New data sets include:
-        ``ztf_alerts``
-        ``testing_dataset``
-
-    New buckets include:
-        ``<PROJECT_ID>_ztf_alert_avro_bucket``
-        ``<PROJECT_ID>_testing_bucket``
-
-    New topics include:
-        ``ztf_alerts_in_BQ``
-        ``test_alerts_in_BQ``
-
-    New sinks include:
-        ``broker_logging_sink``
-    """
-
-    setup_big_query()
-    setup_buckets()
-    setup_pubsub()
-    setup_logging_sinks()
